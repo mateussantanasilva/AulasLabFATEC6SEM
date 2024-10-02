73c6f5396f4445a58642e1e510257e35ab8a64e6
mateussantanasilva <santanasilva1778@gmail.com>
2024-10-02 11:38:28 -0300

    :tada: Adicionar Update/Delete: Firebase Firestore

--- projeto-firebase/src/routes.ts
+++ projeto-firebase/src/routes.ts
@@ -14,7 +14,10 @@ router.get('/consulta', async (req, res) => {
   const clientList: DocumentData[] = []
 
   clients.forEach((client) => {
-    clientList.push(client.data())
+    clientList.push({
+      id: client.id,
+      ...client.data(),
+    })
   })
 
   res.render('list', { clientList })
@@ -28,6 +31,51 @@ router.post('/cadastrar', async (req, res) => {
     .add({ nome, telefone, origem, data_contato, observacao })
 
   res.redirect('/')
+})
+
+router.get('/editar/:id', async (req, res) => {
+  const id = req.params.id
+
+  const client = (await db.collection('clientes').doc(id).get()).data()
+
+  const options = [
+    {
+      value: 'celular',
+      label: 'Celular',
+      selected: client?.origem === 'celular',
+    },
+    {
+      value: 'whatsapp',
+      label: 'Whatsapp',
+      selected: client?.origem === 'whatsapp',
+    },
+    {
+      value: 'telefone_fixo',
+      label: 'Telefone Fixo',
+      selected: client?.origem === 'telefone_fixo',
+    },
+  ]
+
+  res.render('edit', { client: { id, ...client }, options })
+})
+
+router.post('/editar', async (req, res) => {
+  const { id, nome, telefone, origem, data_contato, observacao } = req.body
+
+  await db
+    .collection('clientes')
+    .doc(id)
+    .set({ nome, telefone, origem, data_contato, observacao })
+
+  res.redirect('/consulta')
+})
+
+router.get('/excluir/:id', async (req, res) => {
+  const id = req.params.id
+
+  await db.collection('clientes').doc(id).delete()
+
+  res.redirect('/consulta')
 })
 
 export { router }
